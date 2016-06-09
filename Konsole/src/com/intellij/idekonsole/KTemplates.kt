e6d8d38d23b6104e3d8b269b6b30b0a8154d056f
Aleksey Pivovarov <AMPivovarov@gmail.com>
2016-06-09 16:46:54 +0300

    restore scripting import in template

--- Konsole/src/com/intellij/idekonsole/KTemplates.kt
+++ Konsole/src/com/intellij/idekonsole/KTemplates.kt
@@ -18,6 +18,7 @@ object KTemplates {
 
             import com.intellij.idekonsole.results.*
             import com.intellij.idekonsole.runtime.*
+            import com.intellij.idekonsole.scripting.*
 
             ${FOLDING_START}fun main_exec(): KResult {
                 ${CARET}return KStdoutResult("Hello World")
