5758675257e8d67a438d738511bf5c5dd59260f3
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2016-06-09 19:56:20 +0300

    add import to JavaTokenType to simplify completion

--- Konsole/src/com/intellij/idekonsole/KTemplates.kt
+++ Konsole/src/com/intellij/idekonsole/KTemplates.kt
@@ -19,6 +19,7 @@ object KTemplates {
             import com.intellij.idekonsole.results.*
             import com.intellij.idekonsole.runtime.*
             import com.intellij.idekonsole.scripting.*
+            import com.intellij.psi.JavaTokenType
 
             ${FOLDING_START}fun main_exec(): KResult? {
                 ${CARET}return KStdoutResult("Hello World")
