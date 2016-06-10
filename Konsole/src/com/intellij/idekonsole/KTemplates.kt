13943bbbbd02173982d5263e80875308dc584e6e
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2016-06-10 08:28:45 +0300

    add import to isNullLiteral()

--- Konsole/src/com/intellij/idekonsole/KTemplates.kt
+++ Konsole/src/com/intellij/idekonsole/KTemplates.kt
@@ -18,6 +18,7 @@ object KTemplates {
             import com.intellij.idekonsole.results.*
             import com.intellij.idekonsole.runtime.*
             import com.intellij.idekonsole.scripting.*
+            import org.jetbrains.kotlin.j2k.*
 
             fun main_exec(){
             show{
